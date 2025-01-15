165dd98e7ee085178c5acaa4322df759d9663bce
Cryyufstal <86175191+Cryyufstal@users.noreply.github.com>
2025-01-15 01:51:58 +0100

    Update BottomNavigation.tsx

--- components/BottomNavigation.tsx
+++ components/BottomNavigation.tsx
@@ -1,16 +1,12 @@
-'use client';
+// components/BottomNavigation.tsx
 
 import Link from 'next/link';
 
 export default function BottomNavigation() {
   return (
-    <div className="fixed bottom-0 left-0 right-0 bg-gray-800 text-white p-4 flex justify-around">
-      <Link href="/" className="text-lg font-bold hover:underline">
-        Home
-      </Link>
-      <Link href="/second-page" className="text-lg font-bold hover:underline">
-        Second Page
-      </Link>
+    <div className="fixed bottom-0 left-0 w-full bg-blue-500 p-4 text-white flex justify-around">
+      <Link href="/" className="text-center">Home</Link>
+      <Link href="/tasks" className="text-center">Tasks</Link> {/* رابط صفحة التاسكات */}
     </div>
   );
 }
